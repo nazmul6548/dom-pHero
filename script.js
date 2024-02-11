74f57d7bcfde66de831479f2c66279dd4de68895
nazmul jewel <nazmul.islam.juwel9@gmail.com>
2024-02-11 19:02:41 +0600

    practice

--- script.js
+++ script.js
@@ -12,3 +12,56 @@ mytagName.style.color="green";
 mytagName.style.fontSize="100px";
 mytagName.style.padding="20px";
 mytagName.style.textAlign="center";
+
+// const body = document.getElementById('bodys');
+// const div = document.createElement('div');
+// // div.innerText="nazmul"
+// const addText = document.createElement('h1');
+// addText.innerText="this is title"
+// div.appendChild(addText);
+// const para = document.createElement('p');
+// para.innerText ="first of all our educational articles";
+// div.appendChild(para);
+// const ul = document.createElement('ul');
+// div.appendChild(ul);
+// const li1 = document.createElement('li');
+// li1.innerText ="about";
+// ul.appendChild(li1);
+// // 
+// const li2 = document.createElement('li');
+// li2.innerText ="adress";
+// ul.appendChild(li2);
+// // 
+// const li3 = document.createElement('li');
+// li3.innerText ="skill";
+// ul.appendChild(li3);
+// // 
+// const li4 = document.createElement('li');
+// li4.innerText ="contact";
+// ul.appendChild(li4);
+
+
+// body.appendChild(div);
+
+const bodyt = document.getElementById('bodys');
+
+const div = document.createElement('div');
+div.innerHTML = `
+          <h1> this is tittle </h2>
+          <p>lorem ipsum dolor sit am </p>
+          <ul>
+          <li>home</li>
+          <li>about</li>
+          <li>skill</li>
+          </ul>
+`
+div.style.backgroundColor ="red";
+div.style.padding ="10px"
+
+
+bodyt.appendChild(div);
+
+
+function myFunc() {
+    document.write('new addition')
+}
