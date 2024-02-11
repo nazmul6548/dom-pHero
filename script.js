0037c7822ae017e699e93b67f32d80c40cb91565
nazmul jewel <nazmul.islam.juwel9@gmail.com>
2024-02-11 16:19:40 +0600

    practice

--- script.js
+++ script.js
@@ -6,3 +6,9 @@ for (let i = 0; i < result.length; i++) {
     console.log(result[i].innerText="jishani");
     
 }
+const mytagName = document.getElementById('nameId');
+mytagName.style.backgroundColor="red";
+mytagName.style.color="green";
+mytagName.style.fontSize="100px";
+mytagName.style.padding="20px";
+mytagName.style.textAlign="center";
